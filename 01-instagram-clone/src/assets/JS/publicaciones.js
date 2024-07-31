61512f45aa6fa07873133a2dd95e5bd834508921
MTJonathan <jonathan02330@gmail.com>
2024-07-31 12:39:11 -0500

    Ig Ajustes

---
+++ 01-instagram-clone/src/assets/JS/publicaciones.js
@@ -0,0 +1,53 @@
+export const publicacionesArray = [
+  {
+    img: "https://unavatar.io/luisitoeIpillo",
+    user: "luisitoComunica",
+    lugar: "CDMX",
+    imgPrincipal: "https://pbs.twimg.com/media/DsUnUPZW0AAbhjV.jpg:large",
+    tiempo: "1 hora",
+  },
+  {
+    img: "https://unavatar.io/acampossalazar",
+    user: "acampossalazar",
+    lugar: "Lima",
+    imgPrincipal: "https://i.pinimg.com/originals/30/28/0d/30280d34399ea2bb5effce0f55f705f7.jpg",
+    tiempo: "2 Meses",
+  },
+  {
+    img: "https://unavatar.io/FCBarcelona_es",
+    user: "FCBarcelona",
+    imgPrincipal: "https://cdn2.mediotiempo.com/uploads/media/2024/05/21/alexia-putellas-renovo-barcelona-ig.jpg",
+    tiempo: "1 Año",
+  },
+  {
+    img: "https://unavatar.io/paucubarsi33",
+    user: "PauCubarsi", 
+    imgPrincipal: "https://i0.wp.com/produccionmercurio.s3.amazonaws.com/wp-media-folder-diario-el-mercurio/wp-content/uploads/2024/03/barca-3.jpg?fit=680%2C680&ssl=1",
+    tiempo: "1 Año",
+  },
+  {
+    img: "https://unavatar.io/luanalonsom",
+    user: "luanalonsom",
+    imgPrincipal: "https://pbs.twimg.com/media/Fi2fPffWAAEdWKF?format=jpg&name=large",
+    tiempo: "2 Año",
+  },
+  {
+    img: "https://unavatar.io/itextosoff",
+    user: "itextosoff",
+    imgPrincipal: "https://pbs.twimg.com/media/GTsqDF_XoAAQ8EQ?format=jpg&name=900x900",
+    tiempo: "2 Año",
+  },
+  {
+    img: "https://unavatar.io/itshannaowo",
+    user: "itshannaowo",
+    imgPrincipal: "https://bolavip.com/__export/1667401383079/sites/bolavip/img/2022/11/02/hannahowo.jpg_1216690859.jpg",
+    lugar: "USA",
+    tiempo: "5 Horas",
+  },
+  {
+    img: "https://unavatar.io/Inspiraas",
+    user: "🪐",
+    imgPrincipal: "https://pbs.twimg.com/media/GTs7hxoW4AA29Ri?format=jpg&name=900x900",
+    tiempo: "10 Minutos",
+  }
+];
--- 01-instagram-clone/src/components/mains/principal/publicacion.jsx
+++ 01-instagram-clone/src/components/mains/principal/publicacion.jsx
@@ -1,59 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./post";
 import "../../../assets/css/mains/principal/publicacion.css";
+import { publicacionesArray } from "../../../assets/JS/publicaciones";
 const Publicacion = () => {
+  const [publicaciones, setPublicaciones] = useState(publicacionesArray);
   return (
     <section className="publicaciones">
-      <Post
-        img={"https://unavatar.io/luisitoeIpillo"}
-        user={"luisitoComunica"}
-        lugar={"CDMX"}
-        imgPrincipal={
-          "https://pbs.twimg.com/media/DsUnUPZW0AAbhjV.jpg:large"
-        }
-        tiempo={"1 hora"}
-      />
-      <Post
-        img={"https://unavatar.io/acampossalazar"}
-        user={"acampossalazar"}
-        lugar={"Lima"}
-        imgPrincipal={
-          "https://i.pinimg.com/originals/30/28/0d/30280d34399ea2bb5effce0f55f705f7.jpg"
-        }
-        tiempo={"2 Meses"}
-      />
-      <Post
-        img={"https://unavatar.io/FCBarcelona_es"}
-        user={"FCBarcelona"}
-        imgPrincipal={
-          "https://cdn2.mediotiempo.com/uploads/media/2024/05/21/alexia-putellas-renovo-barcelona-ig.jpg"
-        }
-        tiempo={"1 Año"}
-      />
-      <Post
-        img={"https://unavatar.io/paucubarsi33"}
-        user={"PauCubarsi"}
-        imgPrincipal={
-          "https://i0.wp.com/produccionmercurio.s3.amazonaws.com/wp-media-folder-diario-el-mercurio/wp-content/uploads/2024/03/barca-3.jpg?fit=680%2C680&ssl=1"
-        }
-        tiempo={"1 Año"}
-      />
-      <Post
-        img={"https://unavatar.io/luanalonsom"}
-        user={"luanalonsom"}
-        imgPrincipal={
-          "https://pbs.twimg.com/media/Fi2fPffWAAEdWKF?format=jpg&name=large"
-        }
-        tiempo={"2 Año"}
-      />
-      <Post
-        img={"https://unavatar.io/itextosoff"}
-        user={"itextosoff"}
-        imgPrincipal={
-          "https://pbs.twimg.com/media/GTsqDF_XoAAQ8EQ?format=jpg&name=900x900"
-        }
-        tiempo={"2 Año"}
-      />
+      {publicaciones.map((publicacion) => (
+        <Post
+          img={publicacion.img}
+          user={publicacion.user}
+          lugar={publicacion.lugar}
+          imgPrincipal={publicacion.imgPrincipal}
+          tiempo={publicacion.tiempo}
+        />
+      ))}
       <div className="espacio">
         <br /><br />
       </div>
