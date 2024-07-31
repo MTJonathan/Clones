a8fb65baeae30f076c6a91b6a42684c12b79b548
MTJonathan <jonathan02330@gmail.com>
2024-07-30 22:20:15 -0500

    Ig Corazon

--- 01-instagram-clone/src/components/mains/principal/postBtn.jsx
+++ 01-instagram-clone/src/components/mains/principal/postBtn.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Notificaciones, Mensaje, IconoCompartir, GuardadosSvg } from '../../navegacion/iconos'
+import React, { useState } from 'react'
+import { Notificaciones, Mensaje, IconoCompartir, GuardadosSvg, CorazonActiveSvg } from '../../navegacion/iconos'
 const PostBtn = () => {
+    const [corazon, setCorazon] = useState(false)
+    const classCorazon = corazon ? 'corazonActive' : 'corazonDesabled'
+    const classCorazon2 = corazon ? 'corazonDesabled' : 'corazonActive'
   return (
     <div className="postBtn">
       <div className="principales">
-        <span className='corazon'>
-            <span className='corazonDesabled'><Notificaciones /></span>
-            <span className='corazonActive'></span>
+        <span className='corazon' onClick={() => setCorazon(!corazon)}>
+            <span className={classCorazon2}><Notificaciones /></span>
+            <span className={classCorazon + ' corazonFill'}><CorazonActiveSvg /></span>
         </span>
         <IconoCompartir />
         <Mensaje />
--- 01-instagram-clone/src/components/navegacion/iconos.jsx
+++ 01-instagram-clone/src/components/navegacion/iconos.jsx
@@ -120,3 +120,6 @@ export const GuardarSvg = () => {
         <svg aria-label="Guardar" class="x1lliihq x1n2onr6 x5n08af Guardadas" fill="currentColor" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Guardar</title><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
     )
 }
+export const CorazonActiveSvg = () => {
+    return <svg aria-label="Ya no me gusta" class="x1lliihq x1n2onr6 xxk16z8" fill="currentColor" height="24" role="img" viewBox="0 0 48 48" width="24"><title>Ya no me gusta</title><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
+}
